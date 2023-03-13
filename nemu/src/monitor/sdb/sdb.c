68c33c9e2e605559f4c1b15457c9bd8cc013dfc3
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-13 16:44:13 +0800

    > compile NEMU ysyx_22040000 zhou hong gang Linux zhg-ysyx 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:44:13 up  3:04,  1 user,  load average: 0.18, 0.43, 0.51

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -89,7 +89,7 @@ static int cmd_x(char *args){
   sscanf(N,"%d",&n2);
   sscanf(EXPR,"%lx",&addr1);
   for(int i=0; i<n2; i++){
-    printf("0x%08lx:%lx\n",addr1, vaddr_read(addr1,4));
+    printf("0x%08lx:%08lx\n",addr1, vaddr_read(addr1,4));
     addr1 += 4;
   }
   return 0;
