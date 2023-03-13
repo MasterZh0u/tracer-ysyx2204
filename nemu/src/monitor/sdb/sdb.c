6f757d33af004eee642753cc84ea8257afe1fb24
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-13 14:45:47 +0800

    > compile NEMU ysyx_22040000 zhou hong gang Linux zhg-ysyx 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:45:47 up  1:05,  1 user,  load average: 0.63, 0.55, 0.49

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -71,6 +71,10 @@ static int cmd_si(char *args){
   return 0;
 }
 
+static int cmd_info(char *args){
+  return 0;
+}
+
 static int cmd_help(char *args);
 
 static struct {
@@ -81,8 +85,8 @@ static struct {
   { "help", "Display information about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
-  { "si", "Exit N instructions then nemu_stata parse to STOP", cmd_si},
-  {},
+  { "si", "Exit N instructions then nemu_stata parse to STOP", cmd_si },
+  { "info", "Print the information of registers", cmd_info },
   {},
   {},
   {},
